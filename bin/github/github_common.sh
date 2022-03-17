458dcbfa6e7bb23259f5f893844587f682bd95b8
Helenka Casler <hcasler@gradcenter.cuny.edu>
2022-03-17 18:14:22 -0400

    moved the cmsbot code to github_common

--- bin/github/github_common.sh
+++ bin/github/github_common.sh
@@ -2,6 +2,87 @@
 # Helenka Casler, 2021
 # Contact: HCasler on GitHub
 
+# cmsbot uses code from Mu2e/CI to post on GitHub and get PR info
+function setup_cmsbot() {
+    export CMS_BOT_DIR="$WORKSPACE/CI"
+
+    [ -d "$HOME/mu2e-gh-bot-venv/" ] || python3 -m venv $HOME/mu2e-gh-bot-venv/
+    source $HOME/mu2e-gh-bot-venv/bin/activate
+    CMS_BOT_VENV_SOURCED=1
+
+    python --version
+
+    if [ ! -d ${CMS_BOT_DIR} ]; then
+        (
+            cd "$WORKSPACE"
+            git clone git@github.com:Mu2e/CI
+        )
+    else
+        (
+            cd ${CMS_BOT_DIR}
+            # make sure we don't try to use the old "master" branch, in favor
+            # of "main"
+            if [[ "$(git rev-parse --abbrev-ref HEAD)" == *"master"* ]]; then
+                git fetch
+                git checkout main
+                git pull
+            fi
+            git reset --hard HEAD
+            git fetch; 
+            git pull
+            cd -
+        )
+    fi
+    pip install -U -r ${CMS_BOT_DIR}/requirements.txt
+    pip freeze
+}
+
+function cmsbot_report() {
+    if [ "${CMS_BOT_VENV_SOURCED}" -ne 1 ]; then
+        CMS_BOT_VENV_SOURCED=1
+        source $HOME/mu2e-gh-bot-venv/bin/activate
+    fi
+    ${CMS_BOT_DIR}/comment-github-pullrequest -r ${REPOSITORY} -p ${PULL_REQUEST} --report-file $1
+}
+
+function cmsbot_report_test_status() {
+    if [ "${CMS_BOT_VENV_SOURCED}" -ne 1 ]; then
+        CMS_BOT_VENV_SOURCED=1
+        source $HOME/mu2e-gh-bot-venv/bin/activate
+    fi
+
+    ${CMS_BOT_DIR}/report-test-status \
+        --repository ${REPOSITORY} \
+        --pullrequest ${PULL_REQUEST} \
+        --commit "${COMMIT_SHA}" \
+        --test-name "$1" \
+        --test-state "$2" \
+        --message "$3" \
+        --url "$4"
+}
+
+function cmsbot_write_pr_base() {
+    # args: $1 is repo, $2 is pr number, $3 is the sha file name
+    #       $4 is optional; if provided, it the function writes the
+    #       base branch name instead of the commit sha. If not provided, 
+    #       it defaults to writing the commit sha.
+    # example: cmsbot_write_pr_base Mu2e/Offline 581 repoOffline_pr581_baseSha.txt (writes sha)
+    # example: cmsbot_write_pr_base Mu2e/Offline 581 repoOffline_pr581_baseName.txt True (writes branch name)
+    # (That "True" is just a string to bash, but gets passed to Python, which expects a boolean)
+    if [ "${CMS_BOT_VENV_SOURCED}" -ne 1 ]; then
+        CMS_BOT_VENV_SOURCED=1
+        source $HOME/mu2e-gh-bot-venv/bin/activate
+    fi
+    just_name=${4:-NOARG} # just the string "NOARG"
+    if [ $just_name == NOARG ]; then
+        ${CMS_BOT_DIR}/get-pr-base-sha -r $1 -p $2 -f $3
+    else
+        ${CMS_BOT_DIR}/get-pr-base-sha -r $1 -p $2 -f $3 -j $just_name
+    fi
+}
+
+
+
 # add a row to the test statuses
 function append_report_row() {
     MU2E_POSTTEST_STATUSES="${MU2E_POSTTEST_STATUSES}
